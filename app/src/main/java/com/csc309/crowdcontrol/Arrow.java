3c4354e914eed7d7bf10fc95ec165781b37cf934
Spencer Schurk <spencer.schurk@gmail.com>
2018-06-03 21:46:51 -0700

    Removed smells from Arrow.java

--- app/src/main/java/com/csc309/crowdcontrol/Arrow.java
+++ app/src/main/java/com/csc309/crowdcontrol/Arrow.java
@@ -13,7 +13,9 @@ public class Arrow extends GameObject {
         LEFT, RIGHT, UP, DOWN
     }
 
-    private int x, y, velocity, screenWidth, screenHeight;
+    private int x;
+    private int y;
+    private int screenHeight;
     DIRECTION mode;
     private Bitmap image;
     private boolean shouldDelete;
@@ -21,64 +23,25 @@ public class Arrow extends GameObject {
     //USED TO TRACK SCREEN POSITION
     private float songPosStart;
     private float songPosTarget;
-    private boolean bradArrow = false;
     public float currentSongPos;
 
-    int ARROW_STARTING_Y; //Where arrows are spawned
-    private float ARROW_TARGET_Y; //Where arrows should end up
-
-    public Arrow(Context current, DIRECTION mode, int velocity, int screenWidth, int screenHeight) {
-        y = 0;
-        this.velocity = velocity;
-        this.screenWidth = screenWidth;
-        this.screenHeight = screenHeight;
-        this.mode = mode;
-
-        Bitmap bmp = BitmapFactory.decodeResource(current.getResources(), R.drawable.uparrowbluenormal);
-        bmp = Bitmap.createScaledBitmap(bmp, screenWidth/6, screenWidth/6, false);
-
-
-        if (mode == DIRECTION.LEFT) {
-            // Left Arrow
-            image = rotateBitmap(bmp, 270);
-            x = screenWidth/15; // left gap
-        }
-        else if (mode == DIRECTION.UP) {
-            // Up Arrow
-            image = bmp;
-            x = 2 * screenWidth/15 + image.getWidth(); // two gaps + 1 width
-        }
-        else if (mode == DIRECTION.DOWN) {
-            // Down Arrow
-            image = rotateBitmap(bmp, 180);
-            x = 3 * screenWidth/15 + 2 * image.getWidth(); // three gaps + 2 widths
-        }
-        else {
-            // Right Arrow
-            image = rotateBitmap(bmp, 90);
-            x = 4 * screenWidth/15 + 3 * image.getWidth(); // four gaps + 3 widths
-        }
-    }
-
-
+    int arrowStartingY; //Where arrows are spawned
+    private float arrowTargetY; //Where arrows should end up
 
     //BRAD IS RESPONSIBLE FOR THIS AND WHATEVER CONSEQUENCES ARISE FROM IT
     public Arrow(Context current, DIRECTION mode, float songPosStart, float songPosTarget,
                  int screenWidth, int screenHeight)
     {
-        ARROW_STARTING_Y = -1000;
-        y = ARROW_STARTING_Y;
-        this.screenWidth = screenWidth;
+        arrowStartingY = -1000;
+        y = arrowStartingY;
         this.screenHeight = screenHeight;
         this.mode = mode;
 
         //Sencer... take a look at this super wack math. Maybe there's a better way to do this
-        this.ARROW_TARGET_Y = 7 * screenHeight / 12 + screenHeight / 24;
+        this.arrowTargetY = 7 * screenHeight / 12 + screenHeight / 24;
 
         this.songPosStart = songPosStart;
         this.songPosTarget = songPosTarget;
-
-        bradArrow = true;
 
         Bitmap bmp = BitmapFactory.decodeResource(current.getResources(), R.drawable.uparrowbluenormal);
         bmp = Bitmap.createScaledBitmap(bmp, screenWidth/6, screenWidth/6, false);
@@ -114,8 +77,7 @@ public class Arrow extends GameObject {
         Matrix matrix = new Matrix();
         // Setup rotation degree
         matrix.postRotate(degree);
-        Bitmap bmp = Bitmap.createBitmap(src, 0, 0, src.getWidth(), src.getHeight(), matrix, true);
-        return bmp;
+        return Bitmap.createBitmap(src, 0, 0, src.getWidth(), src.getHeight(), matrix, true);
     }
 
     public boolean shouldDelete() {
@@ -128,7 +90,7 @@ public class Arrow extends GameObject {
 
 
         float temp = (songPosTarget - currentSongPos)/(songPosTarget - songPosStart);
-        y = ((int)((1-temp) * (ARROW_TARGET_Y - (ARROW_STARTING_Y)))) + (ARROW_STARTING_Y);
+        y = ((int)((1-temp) * (arrowTargetY - (arrowStartingY)))) + (arrowStartingY);
 
         if (y > screenHeight || currentSongPos > songPosTarget) {shouldDelete = true;}
     }
