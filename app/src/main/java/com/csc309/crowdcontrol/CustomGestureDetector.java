7ebf6b29af796494004c2404d0b30a4bc4e1a28a
rocketeer55 <spencer.schurk@gmail.com>
2018-06-05 17:33:50 -0700

    Reduced cognitive complecity of CustomGestureDetector.java

--- app/src/main/java/com/csc309/crowdcontrol/CustomGestureDetector.java
+++ app/src/main/java/com/csc309/crowdcontrol/CustomGestureDetector.java
@@ -43,27 +43,38 @@ public class CustomGestureDetector implements GestureDetector.OnGestureListener 
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
         if (e1.getY() > 9 * screenHeight / 12) {
             // Swipe was in the controller - area
-            if (e1.getX() < screenWidth / 2.f) {
-                // Left half of the screen - check left and right swipes
-                if (e1.getX() - e2.getX() > 50) {
-                    left = true;
-                    return true;
-                }
-                if (e1.getX() - e2.getX() < -50) {
-                    right = true;
-                    return true;
-                }
+            return (checkLeftRight(e1, e2) || checkUpDown(e1, e2));
+        }
+
+        return false;
+    }
+
+    private boolean checkLeftRight(MotionEvent e1, MotionEvent e2) {
+        if (e1.getX() < screenWidth / 2.f) {
+            // Left half of the screen - check left and right swipes
+            if (e1.getX() - e2.getX() > 50) {
+                left = true;
+                return true;
             }
-            else {
-                // right half of the screen - check up and down swipes
-                if (e1.getY() - e2.getY() > 50) {
-                    up = true;
-                    return true;
-                }
-                if (e1.getY() - e2.getY() < -50) {
-                    down = true;
-                    return true;
-                }
+            if (e1.getX() - e2.getX() < -50) {
+                right = true;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean checkUpDown(MotionEvent e1, MotionEvent e2) {
+        if (e1.getX() > screenWidth / 2.f) {
+            // right half of the screen - check up and down swipes
+            if (e1.getY() - e2.getY() > 50) {
+                up = true;
+                return true;
+            }
+            if (e1.getY() - e2.getY() < -50) {
+                down = true;
+                return true;
             }
         }
 
