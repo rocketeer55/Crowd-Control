206b70609a5d12bd2abfdb1ffa57b7c3d26582c0
Spencer Schurk <spencer.schurk@gmail.com>
2018-06-05 19:30:22 -0700

    Started febreezing PlayLevel.java. Febreezed PlayLevelActivity and PlayLevelThread

--- app/src/main/java/com/csc309/crowdcontrol/PlayLevelActivity.java
+++ app/src/main/java/com/csc309/crowdcontrol/PlayLevelActivity.java
@@ -20,19 +20,6 @@ import java.io.FileNotFoundException;
 
 
 public class PlayLevelActivity extends AppCompatActivity {
-
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
     /**
      * Some older devices needs a small delay between UI widget updates
      * and a change of the status and navigation bar.
