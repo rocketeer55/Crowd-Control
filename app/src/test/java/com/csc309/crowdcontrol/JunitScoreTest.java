d98a22106fc7ad52125eb273e87ea85fbe69a872
Spencer Schurk <spencer.schurk@gmail.com>
2018-06-05 21:07:48 -0700

    Fixed tests to reflect change in UpdateScore.java

--- app/src/test/java/com/csc309/crowdcontrol/JunitScoreTest.java
+++ app/src/test/java/com/csc309/crowdcontrol/JunitScoreTest.java
@@ -13,13 +13,13 @@ public class JunitScoreTest {
     }
     @Test
     public void ScoreUpdater_initial() throws Exception {
-        UpdateScore tester = new UpdateScore(200, 200 );
+        UpdateScore tester = new UpdateScore(200);
         int score_tester = tester.score;
         assertEquals(score_tester, 0);
     }
     @Test
     public void ScoreUpdater_Excellent() throws Exception {
-        UpdateScore tester = new UpdateScore(200, 200 );
+        UpdateScore tester = new UpdateScore(200);
         int score_tester = tester.score;
         tester.updateScore(10);
         score_tester = tester.score;
@@ -28,7 +28,7 @@ public class JunitScoreTest {
 
     @Test
     public void ScoreUpdater_Good() throws Exception {
-        UpdateScore tester = new UpdateScore(200, 200 );
+        UpdateScore tester = new UpdateScore(200);
         int score_tester = tester.score;
         tester.updateScore(50);
         score_tester = tester.score;
@@ -36,7 +36,7 @@ public class JunitScoreTest {
     }
     @Test
     public void ScoreUpdater_Okay() throws Exception {
-        UpdateScore tester = new UpdateScore(200, 200 );
+        UpdateScore tester = new UpdateScore(200);
         int score_tester = tester.score;
         tester.updateScore(70);
         score_tester = tester.score;
