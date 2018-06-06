1edab0cfcadf1fcd838424baf6e99f7745b7e274
LarryHu208 <32374419+LarryHu208@users.noreply.github.com>
2018-06-05 18:54:03 -0700

    Junit Tests to publish
    Tests I have 4 Tests

--- app/src/test/java/com/csc309/crowdcontrol/ExampleUnitTest.java
+++ app/src/test/java/com/csc309/crowdcontrol/ExampleUnitTest.java
@@ -1,6 +1,31 @@
 package com.csc309.crowdcontrol;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.provider.MediaStore;
+import android.test.AndroidTestCase;
+import android.test.ServiceTestCase;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.view.ContextThemeWrapper;
+import android.view.Display;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -9,9 +34,42 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+class ExampleUnitTest extends AndroidTestCase {
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void Sequence_Length() throws Exception {
+        Context context = new ContextThemeWrapper();
+        PlayLevel playLevel = new PlayLevel(context);
+        BeatMap beatMap = new BeatMap();
+        Sequencer MyTest = new Sequencer(beatMap, playLevel);
+        float result = MyTest.lengthToFloat(BeatMap.NOTE_LENGTH.WHOLE);
+        assertEquals(result,  result);
+
+    }
+
+    @Test
+    public void ArrowTest() throws Exception {
+        Context context = new ContextThemeWrapper();
+        PlayLevel playLevel = new PlayLevel(context);
+        Arrow arrow = new Arrow(context, Arrow.DIRECTION.DOWN, 5, 200, 200);
+        Boolean result = arrow.shouldDelete();
+        assertFalse(result);
+    }
+    //want to test score counter: if adding properly
+
+    @Test
+    public void MIDItest() throws Exception {
+        MIDIReader midi = new MIDIReader();
+        assertTrue(midi.isEmpty());
+    }
+    //want MIDI incrementer score value to work
+
+    @Test
+    public void
+    //
 }
